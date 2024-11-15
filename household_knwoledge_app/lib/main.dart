4d80a5b59998c65aed2632b69f7aa4247d02288c
Ash <achaudhari@student.ethz.ch>
2024-11-15 16:32:15 +0100

    trial

--- household_knwoledge_app/lib/main.dart
+++ household_knwoledge_app/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'models/task_provider.dart';
 import 'screens/home_screen.dart';
-
+//hi
 void main() {
   runApp(
     MultiProvider(
